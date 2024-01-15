0ad9ab5252a605ba8025df63346d299953b24d56
FrancescoCesari97 <freedus97@gmail.com>
2024-01-15 19:45:20 +0100

    aggiunto javascript

--- main.js
+++ main.js
@@ -1,27 +1,97 @@
-const userNameinput = document.getElementById('user-name')
-const GenderInput = document.getElementById('user-gender')
-const printButton = document.getElementById('print-button')
-const resultElement = document.getElementById('result-element')
+// ! HTML ELEMENTS
 
-// sulla pagina html, al click di un bottone, deve apparire ciao nome,
-//il colore del nome deve essere azzurro se maschio e rosa se femmina 
+const slidesContainerEl = document.getElementById("slides-container");
+const arrowTopEL = document.querySelector(".arrow-top");
+const arrowBotEL = document.querySelector(".arrow-bot");
+const dotsContainerEl = document.querySelector(".carosel-dots");
 
-printButton.addEventListener('click', function(){
+// ! ON LOAD
+// * definisco l'arrey delle slides
+const slides = ["01.webp", "02.webp", "03.webp", "04.webp", "05.webp"];
 
-  const username = userNameinput.value;
+// * definisco la slide mostrata
+let slideIndex = 0;
 
-  const gender = GenderInput.value;
+// * genero la slide
+let slidesHtml = "";
+for (let i = 0; i < slides.length; i++) {
+  const slide = slides[i];
 
-  resultElement.innerText = `ciao  ${username}`;
+  let activeClass = i == slideIndex ? "active" : "";
+  /* if (i == 0) {
+    activeClass = "active";
+  } else {
+    activeClass = "";
+  } */
 
+  slidesHtml += `<img src="./img/${slide}" class="slide ${activeClass}" alt="slide ${i}"/>`;
+}
 
-  if(gender == "Maschile"){
-    resultElement.style.color = "skyblue"
+slidesContainerEl.innerHTML = slidesHtml;
 
-  } else if (gender == "Femminile") {
-    resultElement.style.color = "pink"
+// * genero i dots
+let dotsHtml = "";
+for (let i = 0; i < slides.length; i++) {
+  const activeClass = i == slideIndex ? "active" : "";
+  dotsHtml += `<div class="dot ${activeClass}"></div>`;
+}
+dotsContainerEl.innerHTML = dotsHtml;
 
-  } else{
-    resultElement.style.color = "goldenrod"
+// ! SUL CLICK DI ARROW-NEXT
+
+arrowTopEL.addEventListener("click", function () {
+  // * recupero tutte le slides
+  const allSlides = document.getElementsByClassName("slide");
+  const allDots = document.getElementsByClassName("dot");
+
+  const oldDot = allDots[slideIndex];
+  oldDot.classList.remove("active");
+
+  // * elimino la classe active dalla slide attualmente mostrata
+  const oldSlide = allSlides[slideIndex];
+  oldSlide.classList.remove("active");
+
+  // * incremento slideIndex (indice della slide mostrata)
+  if (slideIndex >= allSlides.length - 1) {
+    slideIndex = 0;
+  } else {
+    slideIndex++;
   }
-})
+
+  // * mostro la nuova slide
+
+  const newSlide = allSlides[slideIndex];
+  newSlide.classList.add("active");
+
+  const newDot = allDots[slideIndex];
+  newDot.classList.add("active");
+});
+
+// ! SUL CLICK DI ARROW-PREV
+arrowBotEL.addEventListener("click", function () {
+  // * recupero tutte le slides
+  const allSlides = document.getElementsByClassName("slide");
+  const allDots = document.getElementsByClassName("dot");
+
+  // * elimino la classe active dalla slide attualmente mostrata
+  const oldSlide = allSlides[slideIndex];
+  oldSlide.classList.remove("active");
+
+  const oldDot = allDots[slideIndex];
+  oldDot.classList.remove("active");
+
+  // * decremento slideIndex (indice della slide mostrata)
+  if (slideIndex <= 0) {
+    slideIndex = allSlides.length - 1;
+  } else {
+    slideIndex--;
+  }
+
+  // * mostro la nuova slide
+
+  const newSlide = allSlides[slideIndex];
+  newSlide.classList.add("active");
+
+  const newDot = allDots[slideIndex];
+  newDot.classList.add("active");
+});
